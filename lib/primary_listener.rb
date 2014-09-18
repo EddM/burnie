9ccb080d792a7655b7c636ee16b4a19170f8fbdc
EddM <eddm@me.com>
2014-09-18 22:11:03 +0100

    daemonize

--- lib/primary_listener.rb
+++ lib/primary_listener.rb
@@ -14,33 +14,36 @@ class PrimaryListener
     @most_recent_comment = Time.now - 300
     @listeners = [LastSeasonStatsListener, CareerStatsListener]
 
-    while true
+    loop do
       @first_comment = nil
 
       begin
-        comments = JSON.parse(open(url).read)
-        sleep(5)
+        pid = Process.fork do
+          comments = JSON.parse(open(url).read)
+          sleep(5)
 
-        comments["data"]["children"].each do |comment|
-          comment = comment["data"]
-          @first_comment = comment unless @first_comment
-          comment_created_at = Time.at(comment["created"]) - 28800
-          break if comment_created_at <= @most_recent_comment
+          comments["data"]["children"].each do |comment|
+            comment = comment["data"]
+            @first_comment = comment unless @first_comment
+            comment_created_at = Time.at(comment["created"]) - 28800
+            break if comment_created_at <= @most_recent_comment
 
-          puts " - Processing comment #{comment["id"]} by #{comment["author"]}"
+            puts " - Processing comment #{comment["id"]} by #{comment["author"]}"
 
-          @listeners.each do |klass|
-            klass.new(@client, comment)
+            @listeners.each do |klass|
+              klass.new(@client, comment)
+            end
           end
+
+          @most_recent_comment = Time.at(@first_comment["created"]) - 28800 if @first_comment
         end
 
-        @most_recent_comment = Time.at(@first_comment["created"]) - 28800 if @first_comment
+        Process.waitpid(pid)
       rescue RedditKit::RateLimited
         puts " - Rate limited. Waiting..."
       end
 
       @first_comment = nil
-
       sleep(SleepTimePerCycle - 5)
     end
   end
