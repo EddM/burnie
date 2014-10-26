e1f06d6ce9c867476f207ab96ee5257aba852ed8
EddM <eddm@me.com>
2014-10-26 23:00:41 +0000

    daemonize

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'redditkit', '~> 1.0.1'
+gem 'daemons'
--- lib/primary_listener.rb
+++ lib/primary_listener.rb
@@ -21,7 +21,7 @@ class PrimaryListener
       @first_comment = nil
 
       begin
-        comments = JSON.parse(open(url).read)
+        comments = JSON.parse open(url).read
         sleep(5)
 
         comments["data"]["children"].each do |comment|
@@ -30,10 +30,21 @@ class PrimaryListener
           comment_created_at = Time.at(comment["created"]) - 28800
           break if comment_created_at <= @most_recent_comment
 
+          if comment["author"] == Configuration["username"]
+            puts " - Ignoring comment #{comment["id"]} by #{comment["author"]}"
+            next
+          end
+
           puts " - Processing comment #{comment["id"]} by #{comment["author"]}"
 
           @listeners.each do |klass|
-            klass.new(@client, comment)
+            begin
+              klass.new(@client, comment)
+            rescue => e
+              puts "  - Exception occured during #{klass}:"
+              puts "      #{e}"
+              puts "      #{e.backtrace.join("\n      ")}"
+            end
           end
         end
 
