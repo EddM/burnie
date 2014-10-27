551f7811b332fd4b2c4d6299210137857ce9eb6a
EddM <eddm@me.com>
2014-10-27 00:34:03 +0000

    crontab config

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'redditkit', '~> 1.0.1'
 gem 'daemons'
+gem 'whenever', :require => false
---
+++ config/schedule.rb
@@ -0,0 +1,13 @@
+# To update crontab:
+# whenever --update-crontab
+
+job_type :burnie, './burnie :task'
+
+every 1.day, :at => "3:00 am" do
+  burnie "gamethread"
+end
+
+every 1.day, :at => "9:00 am" do
+  burnie "schedule"
+  burnie "standings"
+end
