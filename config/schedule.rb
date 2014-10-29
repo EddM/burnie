de30641f935339150b4bd4f87884d024b02871e9
EddM <eddm@me.com>
2014-10-29 09:16:49 +0000

    cd in crontab

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 # To update crontab:
 # whenever --update-crontab
 
-job_type :burnie, '/opt/burnie/burnie :task'
+job_type :burnie, 'cd /opt/burnie && /opt/burnie/burnie :task'
 
 every 1.day, :at => "9:00 am" do
   burnie "gamethread"
