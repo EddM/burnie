95362e625a9460900562613d40d6fa02df0840b4
Edd Morgan <eddm@me.com>
2015-02-23 22:59:36 +0000

    playoff picture task

--- lib/teams.rb
+++ lib/teams.rb
@@ -3,5 +3,16 @@ Teams = {
   "Atlanta" => "ATL",
   "Charlotte" => "CHA",
   "Orlando" => "ORL",
-  "Washington" => "WAS"
+  "Washington" => "WAS",
+  "Toronto" => "TOR",
+  "Chicago" => "CHI",
+  "Cleveland" => "CLE",
+  "Milwaukee" => "MIL",
+  "Brooklyn" => "BKN",
+  "Detroit" => "DET",
+  "Indiana" => "IND",
+  "Boston" => "BOS",
+  "Orlando" => "ORL",
+  "Philadelphia" => "PHI",
+  "New York" => "NY"
 }
