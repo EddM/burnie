dc083482cba96a8017c1941ff403eb1a1c9fd371
Edd Morgan <eddm@me.com>
2016-10-03 22:33:51 +0100

    prune schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,14 +3,10 @@
 
 job_type :burnie, 'cd /opt/burnie && /opt/burnie/burnie :task'
 
-every 30.minutes do
-  burnie "related"
-end
-
 every 1.day, at: "1:00 pm" do
   burnie "gamethread"
-  burnie "playoffs"
-  burnie "summer_league:gamethread"
+  # burnie "playoffs"
+  # burnie "summer_league:gamethread"
 end
 
 every 1.day, at: "9:00 am" do
