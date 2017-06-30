7db1be94f820171b915ecb6a66ca8a6e6b5e4820
Edd Morgan <eddm@me.com>
2017-06-30 23:22:32 +0100

    update schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,7 +6,8 @@ job_type :burnie, 'cd /opt/burnie && /opt/burnie/burnie :task'
 every 1.day, at: "11:00 am" do
   # burnie "gamethread"
   # burnie "playoffs"
-  # burnie "summer_league:gamethread"
+  burnie "summer_league:gamethread"
+  burnie "summer_league:schedule"
 end
 
 every 1.hours do
